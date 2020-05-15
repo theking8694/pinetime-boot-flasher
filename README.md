14ab2be53f5ee9243c00ce18290f7838f09d1645
jlukanc <jlukanc1@gmail.com>
2020-05-15 18:02:05 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# pinetime-boot-flasher
---
+++ flash-boot.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#  Flash Mynewt Bootloader to nRF52 with Raspberry Pi
+
+set -e  #  Exit when any command fails.
+set -x  #  Echo all commands.
+
+echo "Drag the file into the terminal to enter its path or type the file path"
+echo "Make sure the path is correct (should look like ~/Desktop/bootloader.hex / .bin / ???)"
+echo "THE PATH CAN NOT CONTAIN @"
+
+read bootfilepath
+sed --expression "s@bootpath@$bootfilepath@g" flash-boot.ocd flash-boot-replace.ocd
+
+openocd/bin/openocd \
+    -f scripts/nrf52-pi/swd-pi.ocd \
+    -f scripts/nrf52/flash-boot-replace.ocd
